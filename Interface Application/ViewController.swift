5869a7b6dd4b7eea80c137f69aee94346cdb4630
Hutr0 <Hutro-games@yandex.ru>
2020-09-12 13:14:32 +0300

    Initial Commit

---
+++ Interface Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Interface Application
+//
+//  Created by Леонид Лукашевич on 12.09.2020.
+//  Copyright © 2020 Hutr0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
